fc28d1fb3fc18a2d78c67ad21cc5d4548576aeaa
leocar1633 <151679070+leocar1633@users.noreply.github.com>
2024-10-18 22:41:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-Hi!
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL": "bir şakaya karşılık cevap",
+            "SHEESH": "onaylamamak",
+            "CREEPY": "korkunç",
+            "AGGRO": "agresifleşmek/sinirlenmek",
+            }   
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print("kelimenin alnamı :", meme_dict.get(word))
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print("hatalı kelime")
